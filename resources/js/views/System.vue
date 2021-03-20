ab1cee36692741eb677b54ab8f9a607b7d900944
inamumu <qty7.xbcrk@gmail.com>
2021-03-20 12:35:30 +0900

    エラーページ追加

---
+++ resources/js/views/System.vue
@@ -0,0 +1,21 @@
+<template>
+  <main class="main">
+    <div class="main_inner">
+      <section>
+        <div class="head02">
+          <h2>システムエラーが発生しました。</h2>
+        </div>
+      </section>
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../../sass/_variables.scss';
+
+</style>
